21645d6df3483ecc135aff4a2b3b8c410e6f97b9
SIVA LINGAM <sivalingamganaganboyina@gmail.com>
2023-07-22 18:52:45 +0530

    roboshop project script ready

--- rabbit.sh
+++ rabbit.sh
@@ -4,4 +4,4 @@ yum install rabbitmq-server -y
 rabbitmqctl add_user roboshop roboshop123
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
 systemctl enable rabbitmq-server
-systemctl start rabbitmq-server
+systemctl restart rabbitmq-server
