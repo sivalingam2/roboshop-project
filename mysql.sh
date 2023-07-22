5765ae9bd20d6e8f5de7b922bb28f9c6b2acb852
SIVA LINGAM <sivalingamganaganboyina@gmail.com>
2023-07-22 13:13:02 +0530

    roboshop project script ready

--- mysql.sh
+++ mysql.sh
@@ -1,5 +1,5 @@
+cp mysql.repo  /etc/yum.repos.d/mysql.repo
 yum module disable mysql -y
-cp mysql.repo etc/yum.repos.d/mysql.repo
 yum install mysql-community-server -y
 systemctl enable mysqld
 systemctl restart mysqld
