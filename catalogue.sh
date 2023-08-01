95cc08dbbf9e49ee161ef9adecd96f96ebe0fca7
SIVA LINGAM <sivalingamganaganboyina@gmail.com>
2023-08-01 15:32:55 +0530

    heading added to each command

--- catalogue.sh
+++ catalogue.sh
@@ -1,9 +1,9 @@
-echo  -e "\e[32m>>>>> setup nodejs <<<<<\e[0m"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 echo  -e "\e[32m>>>>> catalogue service <<<<<\e[0m"
 cp catalogue.repo /etc/systemd/system/catalogue.service
 echo  -e "\e[32m>>>>> mango repo file <<<<<\e[0m"
 cp mango.repo /etc/yum.repos.d/mongo.repo
+echo  -e "\e[32m>>>>> setup nodejs <<<<<\e[0m"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 echo  -e "\e[32m>>>>> install nodejs <<<<<\e[0m"
 yum install nodejs -y
 echo  -e "\e[32m>>>>> application user <<<<<\e[0m"
@@ -23,6 +23,6 @@ echo  -e "\e[32m>>>>> replace mangodb <<<<<\e[0m"
 mongo --host mangodb.sivadevops22.online </app/schema/catalogue.js
 echo  -e "\e[32m>>>>> load service <<<<<\e[0m"
 systemctl daemon-reload
-echo  -e "\e[32m>>>>> start the service <<<<<\e[0m"
+echo  -e "\e[32m>>>>> restart the service <<<<<\e[0m"
 systemctl enable catalogue
 systemctl restart catalogue
