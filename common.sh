faca6ca1994ac98f62a88298dbefba00fd7f0664
SIVA LINGAM <sivalingamganaganboyina@gmail.com>
2023-08-03 17:33:21 +0530

    functions added

--- common.sh
+++ common.sh
@@ -1,5 +1,5 @@
-greet() {
 log=/tmp/roboshop-log
+greet() {
 echo  -e "\e[32m>>>>> ${component} service <<<<<\e[0m"
 cp ${component}.repo /etc/systemd/system/${component}.service &>>${log}
 echo  -e "\e[32m>>>>> mango repo file <<<<<\e[0m"
@@ -8,26 +8,52 @@ echo  -e "\e[32m>>>>> setup nodejs <<<<<\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log}
 echo  -e "\e[32m>>>>> install nodejs <<<<<\e[0m"
 yum install nodejs -y &>>${log}
-echo  -e "\e[32m>>>>> application user <<<<<\e[0m"
-useradd roboshop &>>${log}
-echo  -e "\e[32m>>>>> setup app directory <<<<<\e[0m"
-rm -rf /app &>>${log}
-echo  -e "\e[32m>>>>> setup app directory <<<<<\e[0m"
-mkdir /app &>>${log}
-echo  -e "\e[32m>>>>> download application <<<<<\e[0m"
-curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>> ${log}
-cd /app &>> ${log}
-unzip /tmp/${component}.zip &>>${log}
+
+ func_apppreq
+
 echo  -e "\e[32m>>>>> download dependencies <<<<<\e[0m"
-cd /app &>>${log}
 npm install &>>${log}
 echo  -e "\e[32m>>>>> install mangodb <<<<<\e[0m"
 yum install mongodb-org-shell -y &>> ${log}
-echo  -e "\e[32m>>>>> replace mangodb <<<<<\e[0m"
+echo  -e "\e[32m>>>>> load schema mangodb <<<<<\e[0m"
 mongo --host mangodb.sivadevops22.online </app/schema/${component}.js &>>${log}
-echo  -e "\e[32m>>>>> load service <<<<<\e[0m"
-systemctl daemon-reload &>>${log}
-echo  -e "\e[32m>>>>> restart the service <<<<<\e[0m"
-systemctl enable ${component} &>>${log}
-systemctl restart ${component} &>>${log}
+
+func_start
+
+}
+func_java() {
+  echo  -e "\e[32m>>>>> install maven <<<<<\e[0m"
+  yum install maven -y
+  echo  -e "\e[32m>>>>> ${component} service <<<<<\e[0m"
+  cp ${component}.repo /etc/systemd/system/${component}.service
+
+ func_apppreq
+ echo  -e "\e[32m>>>>>  build${component} service <<<<<\e[0m"
+  mvn clean package
+  mv target/${component}-1.0.jar ${component}.jar &>> ${log}
+  echo  -e "\e[32m>>>>>  install mysql client <<<<<\e[0m"
+  yum install mysql -y &>> ${log}
+  echo  -e "\e[32m>>>>>  load schema servive <<<<<\e[0m"
+  mysql -h mysql.sivadevops22.online -uroot -pRoboShop@1 < /app/schema/${component}.sql &>> ${log}
+
+  func_start
+}
+func_apppreq(){
+  echo  -e "\e[32m>>>>> application user <<<<<\e[0m"
+  useradd roboshop &>>${log}
+  echo  -e "\e[32m>>>>> clean old content <<<<<\e[0m"
+  rm -rf /app &>>${log}
+  echo  -e "\e[32m>>>>> download application <<<<<\e[0m"
+  mkdir /app &>>${log}
+  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>> ${log}
+  cd /app &>> ${log}
+  unzip /tmp/${component}.zip &>>${log}
+  cd /app &>>${log}
+
+}
+func_start() {
+  echo  -e "\e[32m>>>>> restart ${component} service <<<<<\e[0m"
+  systemctl daemon-reload &>>${log}
+  systemctl enable ${component} &>>${log}
+  systemctl restart ${component} &>>${log}
 }
--- shipping.sh
+++ shipping.sh
@@ -1,15 +1,4 @@
-yum install maven -y
-cp shipping.repo /etc/systemd/system/shipping.service
-useradd roboshop
-mkdir /app
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
-cd /app
-unzip /tmp/shipping.zip
-cd /app
-mvn clean package
-mv target/shipping-1.0.jar shipping.jar
-yum install mysql -y
-mysql -h mysql.sivadevops22.online -uroot -pRoboShop@1 < /app/schema/shipping.sql
-systemctl daemon-reload
-systemctl enable shipping
-systemctl restart shipping
+component=shipping
+source common.sh
+
+func_java
