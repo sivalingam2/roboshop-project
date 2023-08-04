e70548dd26eb3dc471b00da825bb9778756cf318
SIVA LINGAM <sivalingamganaganboyina@gmail.com>
2023-08-04 19:41:26 +0530

    exit status

--- fronted.sh
+++ fronted.sh
@@ -1,20 +1,28 @@
 source common.sh
+
 echo  -e "\e[32m>>>>> install nginx <<<<<\e[0m"
-yum install nginx -y
+yum install nginx -y &>> ${log}
 exit_status
+
 echo  -e "\e[32m>>>>> default content <<<<<\e[0m"
-cp fronted.confi /etc/nginx/default.d/roboshop.conf
+cp fronted.confi /etc/nginx/default.d/roboshop.conf &>> ${log}
+exit_status
+
 echo  -e "\e[32m>>>>> remove the old content <<<<<\e[0m"
+rm -rf /usr/share/nginx/html/* &>> ${log}
 exit_status
-rm -rf /usr/share/nginx/html/*
-exit_status
+
 echo  -e "\e[32m>>>>> download the content <<<<<\e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
-cd /usr/share/nginx/html
-echo  -e "\e[32m>>>>> extract application <<<<<\e[0m"
-unzip /tmp/frontend.zip
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>> ${log}
 exit_status
+
+cd /usr/share/nginx/html &>> ${log}
+
+echo  -e "\e[32m>>>>> extract application <<<<<\e[0m"
+unzip /tmp/frontend.zip &>> ${log}
+exit_status
+
 echo  -e "\e[32m>>>>> restart the service <<<<<\e[0m"
-systemctl enable nginx
-systemctl restart nginx
+systemctl enable nginx &>> ${log}
+systemctl restart nginx &>> ${log}
 exit_status
